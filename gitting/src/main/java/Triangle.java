8ae06efd3f4af1227bd2d14de836e4f5e5b52434
Namish Arora <namish.arora@gmail.com>
2022-09-16 12:14:30 -0400

    equilateral test
    Failed

---
+++ gitting/src/main/java/Triangle.java
@@ -0,0 +1,6 @@
+public class Triangle {
+
+    String kindOfTriangle(int one,int two,int three){
+        return "done";
+    }
+}
--- gitting/src/test/java/typeTest.java
+++ gitting/src/test/java/typeTest.java
@@ -1,2 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
 public class typeTest {
+    Triangle triangle;
+    @Before
+    public void typeTest()
+    {
+        triangle = new Triangle();
+    }
+
+    @Test
+    public void checkEquilateral()
+    {
+        Assertions.assertEquals("Equilateral",triangle.kindOfTriangle(7,7,7));
+    }
+
+
 }
